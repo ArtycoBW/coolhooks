4ccde331da3a9c49a1478b01218a1181c7f21e58
ArtycoBW <artyco.bw@gmail.com>
2024-04-25 08:37:07 +0300

    add inspect and checker plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Inspect from "vite-plugin-inspect";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), Inspect(), checker({ typescript: true })],
+});
